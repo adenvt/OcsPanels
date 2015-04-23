990fbd71cc9aa7591d5dc62becea2769eb1c5e72
adenvt <adenvt@gmail.com>
2015-04-24 06:21:35 +0700

    Rename $app to $f3, And Fix Quick Deposit Route

--- controller/admin/seller.php
+++ controller/admin/seller.php
@@ -7,75 +7,81 @@ class Seller extends \Home {
 	protected
 		$seller;
 
-	function beforeRoute($app) {
-		parent::beforeRoute($app);
-		if ( ! $this->me->isAdmin()) $app->reroute('/logout');
+	function beforeRoute($f3) {
+		parent::beforeRoute($f3);
+		if ( ! $this->me->isAdmin()) $f3->reroute('/logout');
 		$this->seller = new \User;
 	}
 
-	function All($app) {
+	function All($f3) {
 		$sellers = $this->seller->find('type=2');
-		$app->set('sellers',$sellers);
-		$app->set('subcontent','admin/sellers.html');
+		$f3->set('sellers',$sellers);
+		$f3->set('subcontent','admin/sellers.html');
 	}
 
 	function loadSeller() {
-		$app = \Base::instance();
+		$f3 = \Base::instance();
 		$seller = $this->seller;
-		if ($app->exists('PARAMS.id')) {
-			$id = $app->get('PARAMS.id');
+		if ($f3->exists('PARAMS.id')) {
+			$id = $f3->get('PARAMS.id');
 			$seller->load(array('id=? AND type=2',$id));
 			$seller->reroute('/home/admin/seller');
 		}
 		return $seller;
 	}
 
-	function Id($app) {
+	function Id($f3) {
 		$seller = $this->loadSeller();
-		$app->set('seller',$seller);
-		$app->set('subcontent','admin/seller.html');
+		$f3->set('seller',$seller);
+		$f3->set('subcontent','admin/seller.html');
 	}
 
-	function Set($app) {
+	function Set($f3) {
 		$seller = $this->loadSeller();
-		$seller->copyFrom('POST');
-		if ($app->exists('POST.password')) {
-			if ( ! parent::Confirm('POST.password')) {
-				$this->flash('Konfirmasi Password Tidak Cocok');
-				$app->reroute($app->get('URI'));
+		if ($seller->dry()) {
+			$seller->load(array('username=?',$f3->get('POST.username')));
+			if ( ! $seller->dry()) {
+				$this->flash('User sudah terdaftar');
+				$f3->reroute('/home/admin/seller/add');
 			}
-			$password = $app->get('POST.password');
-			$seller->password = \Bcrypt::instance()->hash($password);
+		}
+		$seller->copyFrom('POST');
+		if ($f3->exists('POST.password',$pass)) {
+			if ( ! \Check::Confirm('POST.password')) {
+				$this->flash('Konfirmasi Password Tidak Cocok');
+				$f3->reroute($f3->get('URI'));
+			}
+			$seller->password = $pass;
 		}
 		$seller->save();
 		$this->flash('Berhasil Disimpan','success');
-		$app->reroute('/home/admin/seller/'.$seller->id);
+		$f3->reroute('/home/admin/seller/'.$seller->id);
 	}
 
-	function Lock($app,$data) {
+	function Lock($f3,$data) {
 		$seller = $this->loadSeller();
 		$seller->active = $data['active'];
 		$seller->save();
 		$this->flash('Berhasil Disimpan','success');
-		$app->reroute('/home/admin/seller/'.$seller->id);
+		$f3->reroute('/home/admin/seller/'.$seller->id);
 	}
 
-	function Delete($app) {
-		$seller = $this->Getseller();
+	function Delete($f3) {
+		$seller = $this->loadSeller();
 		$seller->erase();
 		$this->flash('Seller Berhasil Dihapus','success');
-		$app->reroute('/home/admin/seller/');
+		$f3->reroute('/home/admin/seller/');
 	}
 
-	function Deposit($app) {
-		$post 	= $app->get('POST');
+	function Deposit($f3) {
+		$post 	= $f3->get('POST');
 		$seller = $this->seller;
 		$seller->load(array('id=?',$post['id']));
 		$seller->reroute('/home/admin/seller');
 		$seller->saldo = ($seller->saldo + $post['deposit']);
 		$seller->save();
 		$this->flash('Deposit Berhasil','success');
-		$app->reroute('/home/admin/seller');
+		$f3->reroute('/home/admin/seller');
 	}
 
 }
