80473285cdb35521127ed0e971febcc0f536c11c
adenvt <adenvt@gmail.com>
2015-04-30 22:43:02 +0700

    Delete Unused File / Debugging Files

--- index.php
+++ index.php
@@ -3,12 +3,4 @@
 $f3 = require('lib/base.php');
 $f3->config('config/config.ini');
 $f3->config('config/route.ini');
-$f3->route('GET /debug',
-    function() {
-        //header('Content-type: application/json');
-		$test = new \Webmin('128.199.162.70','4d3aku010395');
-		$result = $test->find();
-		var_dump($result);
-    }
-);
 $f3->run();
